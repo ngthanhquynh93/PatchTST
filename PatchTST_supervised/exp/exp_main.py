eb8465cfb915f8c2cec9a3cb2c9ab5ef82685d25
Nguyen Thanh Quynh <ngthanhquynh93@gmail.com>
2024-07-11 13:00:12 +0700

    add dataset and make change exp_main

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -306,9 +306,9 @@ class Exp_Main(Exp_Basic):
         f.write('\n')
         f.close()
 
-        # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe,rse, corr]))
+        np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe,rse, corr]))
         np.save(folder_path + 'pred.npy', preds)
-        # np.save(folder_path + 'true.npy', trues)
+        np.save(folder_path + 'true.npy', trues)
         # np.save(folder_path + 'x.npy', inputx)
         return
 
