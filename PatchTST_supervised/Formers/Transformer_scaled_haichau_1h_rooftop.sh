7ab6e9b95a17d13167705b1e973c29b7c7cd2015
Nguyen Thanh Quynh <ngthanhquynh93@gmail.com>
2024-07-12 15:54:40 +0700

    update script for transformer and Dlinear

---
+++ PatchTST_supervised/Formers/Transformer_scaled_haichau_1h_rooftop.sh
@@ -0,0 +1,43 @@
+# ALL scripts in this file come from Autoformer
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+random_seed=42
+root_path_name=./dataset/
+data_path_name=scaled_haichau_1h_rooftop.csv
+model_id_name=scaled_haichau_1h_rooftop
+data_name=custom
+target=Active_Power
+seq_len=96
+
+for model_name in Autoformer Informer Transformer
+do 
+for pred_len in 1 24 48 72
+do
+  python -u run_longExp.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --seq_len $seq_len \
+    --label_len 1 \
+    --pred_len $pred_len \
+    --e_layers 3 \
+    --d_layers 2 \
+    --factor 1 \
+    --enc_in 1 \
+    --dec_in 1 \
+    --c_out 1 \
+    --des 'Exp' \
+    --itr 1 >logs/LongForecasting/$model_name'_rooftop_'$pred_len.log
+done
+done
---
+++ PatchTST_supervised/scripts/Linear/univariate/scaled_haichau_1h_rooftop.sh
@@ -0,0 +1,34 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+if [ ! -d "./logs/LongForecasting/univariate" ]; then
+    mkdir ./logs/LongForecasting/univariate
+fi
+model_name=DLinear
+root_path_name=./dataset/
+data_path_name=scaled_haichau_1h_rooftop.csv
+model_id_name=scaled_haichau_1h_rooftop
+data_name=custom
+target=Active_Power
+seq_len=96
+pred_len=1
+
+# ETTh1, univariate results, pred_len= 24 48 96 192 336 720
+python -u run_longExp.py \
+  --is_training 1 \
+  --root_path $root_path_name \
+  --data_path $data_path_name \
+  --model_id $model_id_name'_'$pred_len \
+  --model $model_name \
+  --data $data_name \
+  --target $target \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --enc_in 1 \
+  --des 'Exp' \
+  --itr 100 --batch_size 64 --feature S --learning_rate 0.005 >logs/LongForecasting/$model_name'_'fS$model_id_name'_'$pred_len.log
